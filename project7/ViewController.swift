a8fdbf0aa7a4f01d1d6360a093d133dcd9ecd78d
Anıl Avcı <anilavci52@gmail.com>
2022-07-10 21:40:48 +0300

    satırda jsondan bilgiler alındı ve tabloda gösterildi.

--- project7/ViewController.swift
+++ project7/ViewController.swift
@@ -38,8 +38,9 @@ class ViewController: UITableViewController {
     }
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-        cell.textLabel?.text = "Başlık buraya"
-        cell.detailTextLabel?.text = "Altbaşlık burada"
+        let petition = petitions[indexPath.row]
+        cell.textLabel?.text = petition.title
+        cell.detailTextLabel?.text = petition.body
         return cell
     }
 
